56c20daacb3ad4a5c8ceb77d55e91948895fb936
alexa <lexaoliv16@gmail.com>
2025-11-07 13:08:28 +0700

    routes login dan admin

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'pages::index');
+
+$routes->get('/admin', 'pages::admin');
+
+$routes->get('/login', 'pages::login');
--- app/Controllers/pages.php
+++ app/Controllers/pages.php
@@ -8,4 +8,23 @@ class pages extends BaseController
     {
         return view(name: 'hal');
     }
+
+    public function admin()
+    {
+        $data = [
+            'title' => 'Dashboard Admin',
+            'validation' => \Config\Services::validation()
+        ];
+
+        return view('admin/dashboard_admin', $data);
+    }
+    public function login()
+    {
+        $data = [
+            'title' => 'Login',
+            'validation' => \Config\Services::validation()
+        ];
+
+        return view('login/login', $data);
+    }
 }
