1e8a631551a98179cd642c90c8fe5898d98671be
unknown <enzymadona27@gmail.com>
2025-11-14 09:25:15 +0700

    Bagian data user di admin

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -13,14 +13,21 @@ $routes->get('pages', 'Pages::index');
 // Halaman login
 $routes->get('/login', 'bansos::index');
 
-// Halaman admin
-$routes->get('/admin', 'bansos::admin');
-$routes->get('/pengajuan', 'bansos::pengajuan');
+//Adminuser
 
-// CRUD Admin
-$routes->get('/admin/ubah/(:num)', 'bansos::ubah/$1');
-$routes->post('/admin/update/(:num)', 'bansos::update/$1');
-$routes->get('/admin/hapus/(:num)', 'bansos::hapus/$1');
+$routes->get('/adminuser', 'adminuser::index');
+$routes->get('/adminuser/edit/(:num)', 'adminuser::edit/$1');
+$routes->post('/adminuser/update/(:num)', 'adminuser::update/$1');
+$routes->get('/adminuser/hapus/(:num)', 'adminuser::hapus/$1');
+
+// // Halaman admin
+// $routes->get('/admin', 'bansos::admin');
+// $routes->get('/pengajuan', 'bansos::pengajuan');
+
+// // CRUD Admin
+// $routes->get('/admin/ubah/(:num)', 'bansos::ubah/$1');
+// $routes->post('/admin/update/(:num)', 'bansos::update/$1');
+// $routes->get('/admin/hapus/(:num)', 'bansos::hapus/$1');
 
 // Halaman user
 $routes->get('/profil', 'bansos::profil');
